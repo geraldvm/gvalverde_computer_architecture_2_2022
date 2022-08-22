dd37b13ee871c87b9b1bbc54aa2aca7e21dc253b
Gerald Valverde <gerald-_16@hotmail.com>
2022-08-21 21:52:47 -0600

    Multiprocessor

---
+++ Multiprocessor.py
@@ -0,0 +1,9 @@
+import Processor
+
+class Multiprocessor:
+    def __init__(self):
+        self.cpu_01=Processor(1)
+        self.cpu_02=Processor(2)
+        self.cpu_03=Processor(3)
+        self.cpu_04=Processor(4)
+    
