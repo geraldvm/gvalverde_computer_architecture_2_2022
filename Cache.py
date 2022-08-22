7688b771500ed1cd4f76dd543ca8d5d455deba22
Gerald Valverde <gerald-_16@hotmail.com>
2022-08-21 22:36:55 -0600

    Cache Memory Implementation

--- Cache.py
+++ Cache.py
@@ -1,6 +1,7 @@
+import CacheBlock
+
 class Cache:
     def __init__(self,ID):
         self.ID=ID
-        self.BLOCKS=[0,0,0,0]
-
+        self.BLOCKS=CacheBlock(ID)
         print("Cache")
--- CacheBlockUnit.py
+++ CacheBlockUnit.py
@@ -1,8 +1,8 @@
-class BlockUnit:
-    def __init__(self, ID):
-        self.ID=ID
+class CacheBlockUnit:
+    def __init__(self, block_number):
+        self.NUMBER=block_number
         self.COHERENCY='S'
-        self.MEMORY_ADDRESS=ID
+        self.MEMORY_ADDRESS=block_number
         self.DATA=0
     
     def set_data(self,data):
@@ -14,5 +14,8 @@ class BlockUnit:
     def set_coherency(self,state):
         self.COHERENCY=state
     
+    """
+    Estado de coherencia: M (modificado), S (shared), I (inv´alido), E (exclusive).
+    """
     def get_coherency(self):
         return self.COHERENCY
