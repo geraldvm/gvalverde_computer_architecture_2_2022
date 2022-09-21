1d6f9023a96bc0519646c39d0e3a075881cd8d2d
Gerald Valverde <gerald-_16@hotmail.com>
2022-09-20 22:45:36 -0600

    Ca Memory Structure Adjust

--- Cache.py
+++ Cache.py
@@ -3,5 +3,5 @@ import CacheBlock
 class Cache:
     def __init__(self,ID):
         self.ID=ID
-        self.BLOCKS=CacheBlock(ID)
+        self.BLOCKS=[CacheBlock(ID,0),CacheBlock(ID,1),CacheBlock(ID,2),CacheBlock(ID,3)]
         print("Cache")
--- CacheBlock.py
+++ CacheBlock.py
@@ -1,12 +1,30 @@
 import CacheBlockUnit
 
 class CacheBlock:
-    def __init__(self, ID):
+    def __init__(self, ID, SET):
         self.ID=ID
-        self.BLOCKS=[CacheBlockUnit(0),CacheBlockUnit(1),CacheBlockUnit(2),CacheBlockUnit(3)]
+        self.COHERENCY='S'
+        self.SET=SET
+        self.TAG='0000'
+        self.DATA=0
 
     def get_blocks(self):
         return self.BLOCKS
     
     def get_block(self,block_number):
         return self.BLOCKS[block_number]
+    
+    def set_data(self,data):
+        self.DATA=data
+    
+    def get_data(self):
+        return self.DATA
+    
+    def set_coherency(self,state):
+        self.COHERENCY=state
+    
+    """
+    Estado de coherencia: M (modificado), S (shared), I (inv´alido), E (exclusive).
+    """
+    def get_coherency(self):
+        return self.COHERENCY
