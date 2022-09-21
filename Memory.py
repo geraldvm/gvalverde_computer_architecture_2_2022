50f11ebafce958d2687194477d5cbe31c64ecc94
Gerald Valverde <gerald-_16@hotmail.com>
2022-09-20 22:47:29 -0600

    Add new features to memory and add limits validations

--- Memory.py
+++ Memory.py
@@ -1,22 +1,29 @@
 from MemoryBlock import *
+from time import sleep
 
 class Memory:
-    def __init__(self):
+    def __init__(self,timer):
         self.block=[MemoryBlock('0000'),MemoryBlock('0010'),MemoryBlock('0100'),MemoryBlock('0110'),
         MemoryBlock('1000'),MemoryBlock('1010'),MemoryBlock('1100'),MemoryBlock('1110')]
+        self.__timer=timer
 
-    def get_block(self,address):
+    def read(self,address):
         for mem in self.block:
             if(mem.ADDRESS==address):
+                sleep(self.__timer)
                 return mem
         return -1
 
-    def set_block(self,address,data):
+    def write(self,address,data):
+        sleep(self.__timer)
         for mem in self.block:
             if(mem.ADDRESS==address):
                 mem.set_data(data)
                 break
+    
+    def getMemory(self):
+        return self.block
 
-mem= Memory()
-mem.set_block('0000',58)
-print(mem.get_block('0000').get_data())
+#mem= Memory()
+#mem.set_block('0000',58)
+#print(mem.get_block('0000').get_data())
--- MemoryBlock.py
+++ MemoryBlock.py
@@ -1,10 +1,13 @@
 class MemoryBlock:
     def __init__(self,address):
         self.ADDRESS=address
-        self.DATA=0
+        self.DATA=0x00
     
     def get_data(self):
         return self.DATA
     
     def set_data(self,data):
-        self.DATA=data
+        if(data>=0x00 and data<=0x1FFFFFF):
+            self.DATA=data
+        else:
+            print('Error Data out of range')
